b8b2111964d82b8eb31f3bacf3c713dd9b4202fc
Eiki Watanabe <basketball.slamdunk1@icloud.com>
2022-08-19 19:02:04 +0900

    環境構築

---
+++ cmd/monkey/eaters.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func Eat(name string) bool {
+	if name != "" {
+		return false
+	} else {
+		fmt.Println(name)
+		return true
+	}
+}
+
+func main() {
+	var name1 string = "GYUDON"
+	if ok := Eat(name1); !ok {
+		fmt.Println("cannt eat: ", name1)
+	}
+
+	var name2 string = ""
+	if ok := Eat(name2); !ok {
+		fmt.Println(" eat: ", name2)
+	}
+}
